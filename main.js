3719b42a446cc660765ffd3bac5c37f7acc48b86
Sebastian Sanchez <dev.sebastian.sanchez@gmail.com>
2023-09-04 00:36:43 -0300

    Finished mobile view. Tabled and desktop view are next

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ window.addEventListener('scroll', () => {
     document.querySelector('.nav').classList.toggle('window-scroll', window.scrollY > 0)
 })
 
+// Popular section Swiper
 var swiper = new Swiper(".popular__container", {
     spaceBetween: 32,
     grabCursos: true,
@@ -12,7 +13,37 @@ var swiper = new Swiper(".popular__container", {
     loop: 'true',
 
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".navigation-next",
+      prevEl: ".navigation-prev",
     },
   });
+
+// Accordion items
+
+const accordionItems = document.querySelectorAll('.value__accordion--item')
+
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector('.value__accordion--header')
+
+  accordionHeader.addEventListener('click', () => {
+    const openItem = document.querySelector('.accordion--open')
+
+    toggleItem(item)
+
+    if(openItem && openItem!== item) {
+      toggleItem(openItem)
+    }
+  })
+
+})
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector('.value__accordion--content')
+
+  if(item.classList.contains('accordion--open')) {
+    accordionContent.removeAttribute('style');
+    item.classList.remove('accordion--open');
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + 'px';
+    item.classList.add('accordion--open');
+  }
+}
